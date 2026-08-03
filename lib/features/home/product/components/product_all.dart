6d847138ba489563e375fd43687992d5e6176576
Emad <emadmarey29@gmail.com>
2026-08-04 00:13:06 +0300

    first commit

--- lib/features/home/product/components/product_all.dart
+++ lib/features/home/product/components/product_all.dart
@@ -19,12 +19,14 @@ class ProductAll extends StatelessWidget {
     return SliverGrid.builder(
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
+        crossAxisSpacing: 5,
+        mainAxisSpacing: 5,
+        mainAxisExtent: 280,
       ),
       itemCount: products.length,
       itemBuilder: (context, index) {
         final product = products[index];
+
         return InkWell(
           onTap: () {
             Navigator.push(
@@ -34,52 +36,88 @@ class ProductAll extends StatelessWidget {
               ),
             );
           },
-          child: Card(
-            clipBehavior: Clip.antiAlias,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: CachedNetworkImage(
-                    imageUrl: product.images[0],
-                    fit: BoxFit.cover,
-                    width: double.infinity,
-                    placeholder: (context, url) => Center(
-                      child: LoadingAnimationWidget.fourRotatingDots(
-                        color: Colors.blue,
-                        size: 20,
+          child: Stack(
+            children: [
+              Card(
+                clipBehavior: Clip.antiAlias,
+                elevation: 5,
+                borderOnForeground: true,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Stack(
+                        children: [
+                          CachedNetworkImage(
+                            imageUrl: product.images[0],
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                            height: double.infinity,
+                            placeholder: (context, url) => Center(
+                              child: LoadingAnimationWidget.fourRotatingDots(
+                                color: Colors.blue,
+                                size: 20,
+                              ),
+                            ),
+                            errorWidget: (context, url, error) {
+                              return Container(
+                                color: Colors.grey[300],
+                                child: const Icon(
+                                  Icons.broken_image,
+                                  size: 50,
+                                  color: Colors.grey,
+                                ),
+                              );
+                            },
+                          ),
+
+                          Positioned(
+                            top: 1,
+                            right: 1,
+                            child: IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.favorite_border,
+                                color: Colors.white,
+                                size: 26,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    errorWidget: (context, url, error) {
-                      return Container(
-                        color: Colors.grey[300],
-                        child: const Icon(
-                          Icons.broken_image,
-                          size: 50,
-                          color: Colors.grey,
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        product.title,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        style: const TextStyle(fontWeight: FontWeight.bold),
+                    Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            product.title,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 4),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('\$${product.price.toStringAsFixed(2)}'),
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(
+                                  Icons.add_shopping_cart,
+                                  size: 25,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
-                      const SizedBox(height: 4),
-                      Text('\$${product.price.toStringAsFixed(2)}'),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         );
       },
