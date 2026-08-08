8de364b688fb84694411be81995505461516e086
Emad <emadmarey29@gmail.com>
2026-08-08 20:09:54 +0300

    first commit

--- lib/features/profile/profile_screen.dart
+++ lib/features/profile/profile_screen.dart
@@ -6,11 +6,53 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Profile',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
+      appBar: AppBar(title: const Text('Profile'), centerTitle: true),
+
+      body: Column(
+        children: [
+          const CircleAvatar(radius: 50, child: Icon(Icons.person, size: 50)),
+
+          const SizedBox(height: 15),
+
+          const Text(
+            'Emad Marri',
+            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          ),
+
+          const Text(
+            'emad@example.com',
+            style: TextStyle(color: Colors.grey),
+          ),
+
+          const SizedBox(height: 30),
+
+          ListTile(
+            leading: const Icon(Icons.person_outline),
+            title: const Text('Edit Profile'),
+            trailing: const Icon(Icons.arrow_forward_ios, size: 16),
+            onTap: () {},
+          ),
+
+          ListTile(
+            leading: const Icon(Icons.favorite_border),
+            title: const Text('Favorites'),
+            trailing: const Icon(Icons.arrow_forward_ios, size: 16),
+            onTap: () {},
+          ),
+
+          ListTile(
+            leading: const Icon(Icons.settings_outlined),
+            title: const Text('Settings'),
+            trailing: const Icon(Icons.arrow_forward_ios, size: 16),
+            onTap: () {},
+          ),
+
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text('Logout'),
+            onTap: () {},
+          ),
+        ],
       ),
     );
   }
